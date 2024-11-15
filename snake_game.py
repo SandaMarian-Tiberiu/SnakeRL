4f99306659e009a23f596e3d87dbb4141d879efd
Sanda Marian-Tiberiu <tiberiusanda@gmail.com>
2024-11-15 18:09:49 +0200

    Direction and body segments

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,19 @@
 import pygame
+from enum import Enum
+from collections import namedtuple
 
 pygame.init()
+
+
+Point = namedtuple("Point", ['x', 'y'])
+BLOCK_SIZE = 20
+
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
 
 
 class SnakeGame:
@@ -14,6 +27,14 @@ class SnakeGame:
 
         pygame.display.set_caption("Snake")
 
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+
+
     def play_step(self):
         pass
 
@@ -24,6 +45,5 @@ if __name__ == "__main__":
     while True:
         game.play_step()
 
-        break
 
     pygame.quit()
